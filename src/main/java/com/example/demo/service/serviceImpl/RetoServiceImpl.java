9adc12f7b9b03569247d50246164a198ef9a23c9
AngelaAcosta31 <angelamaria731@hotmail.com>
2023-01-18 11:00:43 -0500

    Se agrego validaciones de crear y actualizar reto

--- src/main/java/com/example/demo/service/serviceImpl/RetoServiceImpl.java
+++ src/main/java/com/example/demo/service/serviceImpl/RetoServiceImpl.java
@@ -1,5 +1,8 @@
 package com.example.demo.service.serviceImpl;
 
+import com.example.demo.dao.CursoDAO;
+import com.example.demo.dao.EstadoDAO;
+import com.example.demo.dao.MisionDAO;
 import com.example.demo.dao.RetoDAO;
 import com.example.demo.model.Curso;
 import com.example.demo.model.Estado;
@@ -10,10 +13,12 @@ import com.example.demo.service.CursoService;
 import com.example.demo.service.EstadoService;
 import com.example.demo.service.MisionService;
 import com.example.demo.service.RetoService;
+import com.example.demo.util.Validaciones;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Scope("singleton")
@@ -25,13 +30,13 @@ public class RetoServiceImpl implements RetoService {
     RetoDAO retoDAO;
 
     @Autowired
-    MisionService misionService;
+    MisionDAO misionDAO;
 
     @Autowired
-    EstadoService estadoService;
+    EstadoDAO estadoDAO;
 
     @Autowired
-    CursoService cursoService;
+    CursoDAO cursoDAO;
 
     @Override
     public List<Reto> listReto() throws Exception{
@@ -40,8 +45,8 @@ public class RetoServiceImpl implements RetoService {
 
     @Override
     public String registrar(Reto reto) throws Exception {
-        //TODO:FALTAN VALIDACIONES
         try{
+            validacionesCrear(reto);
             retoDAO.save(reto);
             return "Se creo exitosamente el reto.";
         }catch (Exception e){
@@ -55,7 +60,7 @@ public class RetoServiceImpl implements RetoService {
         Mision idMision = null;
         Estado idEstado = null;
         Curso idCurso = null;
-        //TODO:FALTAN VALIDACIONES
+        validacionesActualizar(retoDTO);
         reto = retoDAO.findById(retoDTO.getIdReto()).orElse(null);
         reto.setNombreReto(retoDTO.getNombreReto());
         reto.setDescripcion(retoDTO.getDescripcion());
@@ -64,12 +69,9 @@ public class RetoServiceImpl implements RetoService {
         reto.setFechaLimite(retoDTO.getFechaLimite());
         reto.setEsGrupal(retoDTO.isEsGrupal());
         reto.setNrEstudiatesGrupo(retoDTO.getNrEstudiatesGrupo());
-        idMision = misionService.findById(retoDTO.getIdMision());
-        reto.setMision(idMision);
-        idEstado = estadoService.findById(retoDTO.getIdEstado());
-        reto.setEstado(idEstado);
-        idCurso = cursoService.findById(retoDTO.getIdCurso());
-        reto.setCurso(idCurso);
+        reto.setMision(misionDAO.findById(retoDTO.getIdReto()).orElse(null));
+        reto.setEstado(estadoDAO.findById(retoDTO.getIdEstado()).orElse(null));
+        reto.setCurso(cursoDAO.findById(retoDTO.getIdCurso()).orElse(null));
         retoDAO.save(reto);
         return "Se actualizo el reto.";
     }
@@ -96,5 +98,134 @@ public class RetoServiceImpl implements RetoService {
             throw new Exception("El reto con id: " + idReto + " no existe.");
         }
         return retoDAO.findById(idReto).get();
+    }
+
+    private void validacionesCrear(Reto reto) throws Exception{
+        if(reto.getNombreReto() == null || reto.getNombreReto().trim().equals("")){
+            throw new Exception("Se debe ingresar el nombre del reto.");
+        }
+        if(Validaciones.isStringLenght(reto.getNombreReto(),50)){
+            throw new Exception("El nombre del reto no debe contener más de 50 caracteres.");
+        }
+        if(reto.getDescripcion() == null || reto.getDescripcion().trim().equals("")){
+            throw new Exception("Se debe ingresar una descripción para el reto.");
+        }
+        if(Validaciones.isStringLenght(reto.getDescripcion(), 300)){
+            throw new Exception("La descripción del reto no debe superar los 300 caracteres.");
+        }
+        if(reto.isEsGrupal() && reto.getNrEstudiatesGrupo()<2){
+            throw new Exception("El número de estudiantes por grupo no puede ser menor a 2 si es un reto grupal.");
+        }
+        if(reto.getMaximoIntentos()<1){
+            throw new Exception("El reto debe tener como mínimo 1 intento.");
+        }
+        if(reto.getEstado().getIdEstado() == null){
+            throw new Exception("Se debe ingresar un id estado.");
+        }
+        if(reto.getEstado().getIdEstado()<0){
+            throw new Exception("Se debe ingresar un id estado válido.");
+        }
+        if(estadoDAO.findById(reto.getEstado().getIdEstado()).toString().equals("Optional.empty")){
+            throw new Exception("Se debe ingresar un id estado válido.");
+        }
+        if(reto.getCurso().getIdCurso() == null){
+            throw new Exception("Se debe ingresar un id curso.");
+        }
+        if(reto.getCurso().getIdCurso()<0){
+            throw new Exception("Se debe ingresar un id curso válido.");
+        }
+        if(cursoDAO.findById(reto.getCurso().getIdCurso()).toString().equals("Optional.empty")){
+            throw new Exception("Se debe ingresar un id curso válido.");
+        }
+        if(reto.getMision().getIdMision() == null){
+            throw new Exception("Se debe ingresar un id misión.");
+        }
+        if(reto.getMision().getIdMision()<0){
+            throw new Exception("Se debe ingresar un id misión válido.");
+        }
+        if(misionDAO.findById(reto.getMision().getIdMision()).toString().equals("Optional.empty")){
+            throw new Exception("Se debe ingresar un id misión válido.");
+        }
+        if(reto.getUsuarioCreador()==null || reto.getUsuarioCreador().equals("")){
+            throw new Exception("Debe ingresar el usuario creador.");
+        }
+        if(Validaciones.isStringLenght(reto.getUsuarioCreador(),50)){
+            throw new Exception("El nombre del usuario creador es muy largo, solo puede contener 50 caracteres.");
+        }
+        if(reto.getFechaCreacion()==null ||reto.getFechaCreacion().toString().equals("")){
+            throw new Exception("Debe ingresar una fecha de creación.");
+        }
+        Date fechaActual = new Date();
+        if(reto.getFechaCreacion().compareTo(fechaActual)>0){
+            throw new Exception("No puede ingresar una fecha que aun no ha sucedido.");
+        }
+    }
+
+    private void validacionesActualizar(RetoDTO retoDTO)throws Exception{
+        if(retoDTO.getIdReto() == null){
+            throw new Exception("Se debe ingresar el id del reto que se va a actualizar.");
+        }
+        if(!retoDAO.existsById(retoDTO.getIdReto())){
+            throw new Exception("No existe un reto con ese id.");
+        }
+        if(retoDTO.getNombreReto() == null || retoDTO.getNombreReto().trim().equals("")){
+            throw new Exception("Se debe ingresar el nombre del reto.");
+        }
+        if(Validaciones.isStringLenght(retoDTO.getNombreReto(),50)){
+            throw new Exception("El nombre del reto no debe contener más de 50 caracteres.");
+        }
+        if(retoDTO.getDescripcion() == null || retoDTO.getDescripcion().trim().equals("")){
+            throw new Exception("Se debe ingresar una descripción para el reto.");
+        }
+        if(Validaciones.isStringLenght(retoDTO.getDescripcion(), 300)){
+            throw new Exception("La descripción del reto no debe superar los 300 caracteres.");
+        }
+        if(retoDTO.isEsGrupal() && retoDTO.getNrEstudiatesGrupo()<2){
+            throw new Exception("El número de estudiantes por grupo no puede ser menor a 2 si es un reto grupal.");
+        }
+        if(retoDTO.getMaximoIntentos()<1){
+            throw new Exception("El reto debe tener como mínimo 1 intento.");
+        }
+        if(retoDTO.getIdEstado() == null){
+            throw new Exception("Se debe ingresar un id estado.");
+        }
+        if(retoDTO.getIdEstado()<0){
+            throw new Exception("Se debe ingresar un id estado válido.");
+        }
+        if(estadoDAO.findById(retoDTO.getIdEstado()).toString().equals("Optional.empty")){
+            throw new Exception("Se debe ingresar un id estado válido.");
+        }
+        if(retoDTO.getIdCurso() == null){
+            throw new Exception("Se debe ingresar un id curso.");
+        }
+        if(retoDTO.getIdCurso()<0){
+            throw new Exception("Se debe ingresar un id curso válido.");
+        }
+        if(cursoDAO.findById(retoDTO.getIdCurso()).toString().equals("Optional.empty")){
+            throw new Exception("Se debe ingresar un id curso válido.");
+        }
+        if(retoDTO.getIdMision() == null){
+            throw new Exception("Se debe ingresar un id misión.");
+        }
+        if(retoDTO.getIdMision()<0){
+            throw new Exception("Se debe ingresar un id misión válido.");
+        }
+        if(misionDAO.findById(retoDTO.getIdMision()).toString().equals("Optional.empty")){
+            throw new Exception("Se debe ingresar un id misión válido.");
+        }
+        if(retoDTO.getUsuarioModificador()==null || retoDTO.getUsuarioModificador().equals("")){
+            throw new Exception("Debe ingresar el usuario creador.");
+        }
+        if(Validaciones.isStringLenght(retoDTO.getUsuarioModificador(),50)){
+            throw new Exception("El nombre del usuario creador es muy largo, solo puede contener 50 caracteres.");
+        }
+        if(retoDTO.getFechaModificacion()==null ||retoDTO.getFechaModificacion().toString().equals("")){
+            throw new Exception("Debe ingresar una fecha de creación.");
+        }
+        Date fechaActual = new Date();
+        if(retoDTO.getFechaModificacion().compareTo(fechaActual)>0){
+            throw new Exception("No puede ingresar una fecha que aun no ha sucedido.");
+        }
+
     }
 }
