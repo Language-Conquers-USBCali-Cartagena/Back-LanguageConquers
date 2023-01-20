503eb4a9ddbc6ec5a2ffc46d80854509e0e6e2cc
AngelaAcosta31 <angelamaria731@hotmail.com>
2023-01-20 17:32:55 -0500

    cambios en controller Articulo

--- src/main/java/com/example/demo/controller/ArticulosController.java
+++ src/main/java/com/example/demo/controller/ArticulosController.java
@@ -7,13 +7,14 @@ import com.example.demo.service.ArticulosService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/articulo")
+@RequestMapping(value="/articulo", consumes = MediaType.APPLICATION_JSON_VALUE)
 public class ArticulosController {
     @Autowired
     ArticulosService articulosService;
