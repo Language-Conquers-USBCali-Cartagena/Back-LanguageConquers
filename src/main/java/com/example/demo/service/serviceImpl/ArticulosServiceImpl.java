cc1d1f366ffe5e6f4242f697168455cf72e7c884
AngelaAcosta31 <angelamaria731@hotmail.com>
2023-01-20 18:11:20 -0500

    Cambio de nombre de los paths en el controller de logro

--- src/main/java/com/example/demo/service/serviceImpl/ArticulosServiceImpl.java
+++ src/main/java/com/example/demo/service/serviceImpl/ArticulosServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.demo.service.serviceImpl;
 
 import com.example.demo.dao.ArticulosDAO;
+import com.example.demo.dao.CategoriaDAO;
+import com.example.demo.dao.EstadoDAO;
 import com.example.demo.model.Articulos;
 import com.example.demo.model.Categoria;
 import com.example.demo.model.Estado;
@@ -8,10 +10,12 @@ import com.example.demo.model.dto.ArticulosDTO;
 import com.example.demo.service.ArticulosService;
 import com.example.demo.service.CategoriaService;
 import com.example.demo.service.EstadoService;
+import com.example.demo.util.Validaciones;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Scope("singleton")
@@ -21,10 +25,10 @@ public class ArticulosServiceImpl implements ArticulosService {
     ArticulosDAO articulosDAO;
 
     @Autowired
-    EstadoService estadoService;
+    EstadoDAO estadoDAO;
 
     @Autowired
-    CategoriaService categoriaService;
+    CategoriaDAO categoriaDAO;
 
     @Override
     public String registrar(Articulos articulos) throws Exception {
@@ -50,7 +54,7 @@ public class ArticulosServiceImpl implements ArticulosService {
         articulo.setImagen(articulosDTO.getImagen());
         articulo.setNivelValido(articulosDTO.getNivelValido());
         articulo.setDescripcion(articulosDTO.getDescripcion());
-        idEstado = estadoService.findById(articulosDTO.getIdEstado());
+        idEstado = estadoDAO.findById(articulosDTO.getIdEstado()).orElse(null);
         articulo.setEstado(idEstado);
         //idCategoria = categoriaService.
 
@@ -92,20 +96,51 @@ public class ArticulosServiceImpl implements ArticulosService {
     }
 
     private void validacionesCrear (Articulos articulos) throws Exception{
-        if(articulos.getCategoria().equals(null)){
-            throw new Exception("Debe ingresar una categoría.");
-        }
-        if(articulos.getEstado().equals(null)){
-            throw new Exception("Debe ingresar un estado.");
-        }
-        if(articulos.getNombre().equals(null)){
+
+        if(articulos.getNombre().equals(null) || articulos.getNombre().trim().equals("") ||
+                Validaciones.isStringLenght(articulos.getNombre(), 50)){
             throw new Exception("Debe ingresar un nombre para el artículo.");
         }
-        if(articulos.getDescripcion().equals(null)){
+        if(articulos.getPrecio()<0){
+            throw new Exception("Se debe ingresar el precio del artículo.");
+        }
+        if(articulos.getDescripcion().equals(null)|| articulos.getDescripcion().trim().equals("") ||
+                Validaciones.isStringLenght(articulos.getDescripcion(), 200)){
             throw new Exception("Debe ingresar una descripción para el artículo.");
         }
-        if(articulos.getImagen().equals(null)){
+        if(articulos.getNivelValido()<0){
+            throw new Exception("Se debe ingresar el nivel válido del artículo.");
+        }
+        if(articulos.getImagen().equals(null) || articulos.getImagen().trim().equals("")){
             throw new Exception("Debe ingresar una imagen para el artículo.");
         }
+        if(articulos.getCategoria().equals(null) || articulos.getCategoria().getIdCategoria() <0){
+            throw new Exception("Debe ingresar una categoría válida.");
+        }
+        if(articulos.getEstado().equals(null) || articulos.getEstado().getIdEstado() <0){
+            throw new Exception("Debe ingresar un estado válido.");
+        }
+        if(categoriaDAO.findById(articulos.getCategoria().getIdCategoria()).toString().equals("Optional.empty")){
+            throw new Exception("Debe ingresar un id categoría válido.");
+        }
+        if(estadoDAO.findById(articulos.getEstado().getIdEstado()).toString().equals("Optional.empty")){
+            throw new Exception("Debe ingresar un id estado válido.");
+        }
+        if(articulos.getUsuarioCreador() == null
+                || articulos.getUsuarioCreador().trim().equals("")
+                || Validaciones.isStringLenght(articulos.getUsuarioCreador(),50)){
+            throw new Exception("Se debe ingresar un usuario creador del artículo válido.");
+        }
+        if(articulos.getFechaCreacion() == null
+                || articulos.getFechaCreacion().toString().equals("")){
+            throw new Exception("Se debe ingresar una fecha válida.");
+        }
+        Date fechaActual = new Date();
+        if(articulos.getFechaCreacion().compareTo(fechaActual)>0){
+            throw new Exception("No puede ingresar una fecha que aun no ha sucedido.");
+        }
+
+
+
     }
 }
